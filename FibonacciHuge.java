596b09ca3d8d4a042bc4e7f52b5b8def2a8b7bcd
Alex Kritskiy <akritskiy@gmail.com>
2017-08-05 02:38:23 -0400

    remove "naive" starter method for clarity

--- FibonacciHuge.java
+++ FibonacciHuge.java
@@ -54,21 +54,4 @@ public class FibonacciHuge {
         }
         return result;
     }
-    
-    //"naive" starter method, provided by instructors, kept for comparison and testing at small n's
-    private static long getFibonacciHugeNaive(long n, long m) {
-        if (n <= 1)
-            return n;
-
-        long previous = 0;
-        long current  = 1;
-
-        for (long i = 0; i < n - 1; ++i) {
-            long tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
-        }
-
-        return current % m;
-    }
 }
