4d69d7c60703901dc375a58843a6e7c5b3af807c
Alex Kritskiy <akritskiy@gmail.com>
2017-09-08 01:32:24 -0400

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,115 @@
+//Design a quick sort algorithm to efficiently process arrays with few unique elements.
+//Typically, quick sort uses a two-way partition. Implement a partition method...
+//that separates elements into three partitions: less than, equal to, and greater than pivot.
+
+import java.util.*;
+
+public class Sorting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        
+        quickSort(arr);
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        // //TEST:
+        // int n = (int)(Math.random() * 20 + 1);
+        // int[] a = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     a[i] = (int)(Math.random() * 10 + 1);
+        // }
+        
+        // int[] copy = new int[a.length];
+        // System.arraycopy(a, 0, copy, 0, a.length);
+
+        // System.out.println("Before: ");
+        // System.out.println(Arrays.toString(a));        
+        // System.out.println("After: ");
+        // System.out.println(Arrays.toString(quickSort(a)));        
+        // Arrays.sort(copy);
+        // System.out.println("Copy, sorted by Arrays.sort: ");
+        // System.out.println(Arrays.toString(copy));
+    }
+    
+    //QUICK SORT METHOD
+    private static int[] quickSort(int[] arr) {
+        return quickSort(arr, 0, arr.length - 1);
+    }
+    
+    private static int[] quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return arr;
+        }
+
+        int[] m = partition(arr, left, right);
+        quickSort(arr, left, m[0]);
+        quickSort(arr, m[1], right);
+        return arr;
+    }
+    
+    //THREE-WAY PARTITION METHOD
+    private static int[] partition(int[] arr) {
+        return partition(arr, 0, arr.length - 1);
+    }
+
+    private static int[] partition(int[] arr, int left, int right) {
+        int[] resultArr = {0, 0};
+
+        //Randomize pivot...
+        int k = (int)(Math.random() * (right - left + 1) + left);
+        int temp = arr[left];
+        arr[left] = arr[k];
+        arr[k] = temp;
+        int pivot = arr[left];
+
+        int n = right; //n = rightmost index... used to keep track of the equal-to partition
+        int j = left + 1;
+        for (int i = left + 1; i <= n; i++) {
+            if (arr[i] < pivot) {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                j++;
+            }
+            else if (arr[i] == pivot) {
+                temp = arr[n];
+                arr[n] = arr[i];
+                arr[i] = temp;
+                n--;
+                i--;
+            }
+        }
+        j--;
+
+        if (j == 0) {
+            resultArr[0] = 0;
+        }
+        else {
+            resultArr[0] = j - 1;
+        }
+
+        temp = arr[left];
+        arr[left] = arr[j];
+        arr[j] = temp;
+
+        j++;
+        for (int i = right; i > n; i--) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+
+        resultArr[1] = j;
+        return resultArr;
+    }
+    //END THREE-WAY PARTITION METHOD
+}
