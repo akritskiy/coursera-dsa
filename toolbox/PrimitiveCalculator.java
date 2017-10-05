1076f247283310ec94e75b23e3e7e7839e740276
Alex Kritskiy <akritskiy@gmail.com>
2017-10-05 13:07:36 -0400

    Update PrimitiveCalculator.java

--- toolbox/PrimitiveCalculator.java
+++ toolbox/PrimitiveCalculator.java
@@ -1,6 +1,19 @@
 /*
 Dynamic programming problem: given an integer n and a primitive calculator that can do three operations (+1, *2, *3),
-compute the minimum number of operations needed to obtain n starting from 1.
+compute the minimum number of operations needed to obtain n starting from 1. Then, reconstruct the sequence of the
+number's path from  1 to n. For example:
+
+Input: 5
+Output:
+3
+1 2 4 5
+Explanation: the optimal path from 1 to 5 takes 3 operations: 1 * 2 * 2 + 1.
+
+Input: 10
+Output:
+3
+1 3 9 10
+Explanation: the optimal path from 1 to 10 also takes 3 operations: 1 * 3 * 3 + 1.
 
 The feedback for this solution was: Good job! (Max time used: 0.28/2.25, max memory used: 42102784/536870912.)
 */
