69661a7b76223ab188ff675cc7e3f56207f2f50b
Alex Kritskiy <akritskiy@gmail.com>
2017-10-17 03:08:20 -0400

    minor edits

--- toolbox/DifferentSummands.java
+++ toolbox/DifferentSummands.java
@@ -1,7 +1,8 @@
 /*
-The goal of this problem is to represent a positive integer as a sum of as many distinct positive integers
-as possible. That is, given an integer n, find the max k such that n = a1 + a2 + ... + ak, and no two ai are
-equal. n can be as large as 10^9.
+The goal of this problem is to represent a positive integer as a sum of as many
+distinct positive integers as possible. That is, given an integer n, find the
+max k such that n = a1 + a2 + ... + ak, and no two ai are equal. n can be as
+large as 10^9.
 
 For example:
 Input: 6
@@ -14,7 +15,8 @@ Output:
 3
 1 2 5
 
-The feedback for this problem was: Good job! (Max time used: 0.61/1.50, max memory used: 50421760/536870912.)
+The feedback for this problem was: Good job! (Max time used: 0.61/1.50, max
+memory used: 50421760/536870912.)
 */
 
 import java.util.*;
@@ -33,8 +35,8 @@ public class DifferentSummands{
 	private static List<Integer> optimalSummands(int n) {
 		List<Integer> summands = new ArrayList<Integer>();
 		
-		for (int i=1; i<=n; i++) {
-			if (2*i + 1 > n) {
+		for (int i = 1; i <= n; i++) {
+			if (2 * i + 1 > n) {
 				summands.add(n);
 				break;
 			}
